dc492515e23342f02d6be7a2786b325caf95cda2
icethetic <ammarmuhraf@gmail.com>
2025-09-10 15:43:33 +0700

    Fix: Issue at settings.py

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ammar-muhammad41-footballnews.pbp.cs.ui.ac.id"]
 
-CSRF_TRUSTED_ORIGINS = ["<https://ammar-muhammad41-footballnews.pbp.cs.ui.ac.id>",]
+CSRF_TRUSTED_ORIGINS = ["https://ammar-muhammad41-footballnews.pbp.cs.ui.ac.id",]
 
 
 # Application definition
